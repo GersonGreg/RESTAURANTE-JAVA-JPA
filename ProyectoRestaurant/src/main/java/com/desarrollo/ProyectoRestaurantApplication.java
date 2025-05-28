33def962ccdccbd824d676e585fccf2a37637970
GersonGreg <gerson.alejo@gmail.com>
2025-05-28 18:15:27 -0500

    Mi PRIMER COMMIT DEL PROYECTO RESTAURANTE

---
+++ ProyectoRestaurant/src/main/java/com/desarrollo/ProyectoRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRestaurantApplication.class, args);
+	}
+
+}
